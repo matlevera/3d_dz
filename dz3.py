dbd073c761d2ec0d9133edc6101a4ad726925875
matlevera <100791350+matlevera@users.noreply.github.com>
2023-10-17 17:48:52 +0300

    Update dz3.py

--- dz3.py
+++ dz3.py
@@ -1,100 +1,67 @@
-import pygame
-from matplotlib import pyplot as plt
+from PIL import Image, ImageDraw
 
-# Класс, представляющий многоугольник
-class Polygon:
-    def init(self, vertices):
-        self.vertices = vertices
+def line_clip(segment, polygon):
+    # Получение координат начала и конца отрезка
+    x1, y1, x2, y2 = segment
 
-    def draw(self, screen):
-        pygame.draw.polygon(screen, (0, 0, 255), self.vertices, 1)
+    # Итерация по ребрам многоугольника
+    for i in range(len(polygon)):
+        x_start, y_start = polygon[i]
+        x_end, y_end = polygon[(i + 1) % len(polygon)]
 
-    def clip_segment(self, start, end):
-        t_min = 0
-        t_max = 1
-        
-        dx = end[0] - start[0]
-        dy = end[1] - start[1]
+        # Вычисление вектора нормали ребра
+        normal = (y_end - y_start, x_start - x_end)
 
-        for i in range(-1, len(self.vertices)-1):
-            x1, y1 = self.vertices[i]
-            x2, y2 = self.vertices[i+1]
-            nx = y2 - y1
-            ny = x1 - x2
-            p = dx * nx + dy * ny
-            q1 = (x1 - start[0]) * nx + (y1 - start[1]) * ny
-            q2 = (x2 - start[0]) * nx + (y2 - start[1]) * ny
+        # Вычисление вектора относительной позиции начала и конца отрезка
+        start_point = (x1 - x_start, y1 - y_start)
+        end_point = (x2 - x_start, y2 - y_start)
 
-            if p == 0 and q1 < 0 and q2 < 0:
-                return None, None
-            if p == 0:
-                continue
+        # Вычисление скалярного произведения вектора нормали и векторов относительной позиции
+        start_dot = normal[0] * start_point[0] + normal[1] * start_point[1]
+        end_dot = normal[0] * end_point[0] + normal[1] * end_point[1]
 
-            t = q1 / p
-
-            if p > 0:
-                if t > t_max:
-                    return None, None
-                t_min = max(t_min, t)
+        # Определение положения начала и конца отрезка относительно ребра
+        if start_dot < 0 and end_dot < 0:
+            # Отрезок полностью находится снаружи многоугольника
+            return None
+        elif start_dot >= 0 and end_dot >= 0:
+            # Отрезок полностью находится внутри многоугольника
+            break
+        else:
+            # Отрезок пересекает ребро многоугольника
+            intersection = (start_dot - end_dot) / (start_dot - end_dot + 1e-6)
+            if start_dot < 0:
+                x1 = x_start + intersection * (x1 - x_start)
+                y1 = y_start + intersection * (y1 - y_start)
             else:
-                if t < t_min:
-                    return None, None
-                t_max = min(t_max, t)
-        
-        xA = start[0] + t_min * dx
-        yA = start[1] + t_min * dy
-        xB = start[0] + t_max * dx
-        yB = start[1] + t_max * dy
+                x2 = x_start + intersection * (x2 - x_start)
+                y2 = y_start + intersection * (y2 - y_start)
 
-        return (xA, yA), (xB, yB)
+    # Возвращение отсеченного отрезка
+    return [int(x1), int(y1), int(x2), int(y2)]
 
-# Инициализация Pygame
-pygame.init()
+# Создание изображения с белым фоном
+width = 500
+height = 500
+image = Image.new("RGB", (width, height), "white")
+draw = ImageDraw.Draw(image)
 
-# Создание экрана Pygame
-width, height = 640, 480
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption('Clipping Algorithm')
+# Задание координат многоугольника
+polygon = [(100, 100), (150, 250), (300, 200), (200, 100)]
 
-# Создание объекта многоугольника
-polygon_vertices = [(100, 100), (250, 50), (400, 100), (450, 250), (400, 400), (250, 450), (100, 400), (50, 250)]
-polygon = Polygon(polygon_vertices)
+# Задание координат отрезка
+segment = (50, 200, 250, 400)
 
-# Отображение экрана с использованием Matplotlib
-fig = plt.figure()
-ax = fig.add_subplot(111)
-plt.ion()
-plt.show()
+# Отсечение отрезка многоугольником
+clipped_segment = line_clip(segment, polygon)
 
-# Отслеживание координат отрезка
-xA, yA, xB, yB = None, None, None, None
+# Рисование многоугольника
+draw.polygon(polygon, outline="black")
 
-# Функция обработки событий мыши
-def on_click(event):
-    global xA, yA, xB, yB
-    if event.button == 1:
-        if xA is None and yA is None:
-            xA, yA = event.xdata, event.ydata
-        elif xB is None and yB is None:
-            xB, yB = event.xdata, event.ydata
-            # Отсекаем отрезок
-            segment_start = (xA, yA)
-            segment_end = (xB, yB)
-            clipped_start, clipped_end = polygon.clip_segment(segment_start, segment_end)
-            if clipped_start is not None and clipped_end is not None:
-                pygame.draw.line(screen, (255, 0, 0), clipped_start, clipped_end, 2)
-                pygame.display.flip()
-            xA, yA, xB, yB = None, None, None, None
+# Рисование исходного и отсеченного отрезков
+draw.line(segment, fill="blue", width=2)
+if clipped_segment:
+    draw.line(clipped_segment, fill="red", width=2)
 
-# Основной цикл
-running = True
-while running:
-    screen.fill((255, 255, 255))
-    polygon.draw(screen)
-    pygame.display.flip()
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == plt.events.ButtonRelease:
-            on_click(event)
+# Сохранение изображения
+image.save("clipped_segment.png")
